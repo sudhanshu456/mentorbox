a05725d57ed9efec9d215da1ab7e00b6c365e41a
sudhanshu456 <spsupraj2d@gmail.com>
2020-09-06 11:19:32 +0530

    correction and removing firstname fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -14,13 +14,13 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password','first_name','last_name',)}),
+        (None, {'fields': ('email', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email','first_name','last_name','password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email','password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email',)
